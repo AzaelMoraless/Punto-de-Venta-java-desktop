f8801e68ac07ac71c14ad324ae2e1cae068823c7
Azael <azaelmorales@hotmail.com>
2018-05-19 14:37:40 -0500

    4 commit diseño

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import java.net.URL;
+import java.nio.file.Paths;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primary_stage) throws Exception {
+        URL url = Paths.get("C:\\Users\\Azael\\Documents\\Sistema\\src\\view\\Login.fxml").toUri().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);   
+        primary_stage.setScene(scene);
+        primary_stage.setTitle("Las Aguilas");
+        primary_stage.setResizable(false);
+        primary_stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
